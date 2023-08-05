68a7408c45106925306a1fab48fc79bd6019854c
M.Mary Shakeena <119723872+maryshakeena@users.noreply.github.com>
2023-08-05 11:44:34 +0530

    import java.util.Scanner; public class add{     public static void main(String[] args){         Scanner scan = new Scanner(System.in);         int num1,num2,num3;         num1=scan.nextInt();         num2=scan.nextInt();         num3=num1+num2;         System.out.println(num3);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class add{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num1,num2,num3;
+        num1=scan.nextInt();
+        num2=scan.nextInt();
+        num3=num1+num2;
+        System.out.println(num3);
+    }
+}
