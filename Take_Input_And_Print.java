9dad59ab248e54351a7803b2fd0bede15769f28c
M.Mary Shakeena <119723872+maryshakeena@users.noreply.github.com>
2023-08-10 11:56:05 +0530

    import java.util.Scanner; public class Sample {     public static void main(String[] args){         Scanner scan = new Scanner(System.in);         String s = scan.nextLine();         System.out.println(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Sample
+{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.println(s);
+    }
+}
