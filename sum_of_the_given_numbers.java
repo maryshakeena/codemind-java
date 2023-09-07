3a7748700b2f2dfad8e7ee23f59188f5059461a8
M.Mary Shakeena <119723872+maryshakeena@users.noreply.github.com>
2023-09-07 10:00:23 +0530

    import java.util.Scanner; public class Sample{     public static void main(String[] args){         Scanner scan=new Scanner(System.in);         int n;         n=scan.nextInt();         for(int i=1;i<=n;i++)         {           int a=scan.nextInt();           int b=scan.nextInt();         int sum=a+b;         System.out.println(sum);
        }
      }
    }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Sample{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int n;
+        n=scan.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+          int a=scan.nextInt();
+          int b=scan.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+        
+    }
+  }
+}
